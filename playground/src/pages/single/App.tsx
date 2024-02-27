5b1c4f85a1386131b468f930b192cdc7b258ba3d
m4rs <marcel@marcelclaramunt.cat>
2024-02-27 20:00:29 +0100

    new use-reorder

--- playground/src/pages/single/App.tsx
+++ playground/src/pages/single/App.tsx
@@ -7,8 +7,8 @@ export function App() {
 
   const items: Item[] = [...range(3)].map(i => ({
     id: `${i}`, elem: ({ idx }: ItemProps) => (
-      <VStack align='center' h='25vh' w='25vh' pos='relative'>
-        <Image maxH='100%' src={`/images/sheet${i % 2}.jpg`} />
+      <VStack align='center' h='25vh' w='25vh' pos='relative' bg={`gray.${i*100+300}`}>
+        {/* <Image maxH='100%' src={`/images/sheet${i % 2}.jpg`} /> */}
         <Flex pos='absolute' top={0} left={0} w='100%' h='100%' align='center' justify='center'>
           <Text fontSize='2rem'>{idx}</Text>
         </Flex>
@@ -18,16 +18,18 @@ export function App() {
   
   const { reorderer, run } = useAnimatedReorder(items, {
     timeForLongPress: 0, forcePressThreshold: 0,
-    // animation: { handIcon }
+    animation: { modalStyle: {
+      borderRadius: 0, background: 'transparent'
+    }}
   })
 
 
   return (
     <VStack h='100vh' w='100vw' justify='center' align='center'>
-      <VStack h='90%' pos='relative' border='1px solid red'>
+      <VStack h='80%' pos='relative'>
         {reorderer}
       </VStack>
-      <Button onClick={run}>Run</Button>
+      <Button onClick={run}>Animation</Button>
     </VStack>
   )
 }
--- use-reorder/README.md
+++ use-reorder/README.md
@@ -1,10 +1,13 @@
 # Use Reorder
 
-> Hooks API wrapper around `react-beautiful-dnd`
+> Simple hooks-based list reorder running on `react-beautiful-dnd`
 
-## Hooks API
 
-Same result, but simple code.
+Your usual reorderer       |  Optional hint animation
+:-------------------------:|:-------------------------:
+![Reorderer in action](media/reorder.gif)  |  ![Reorder animation](media/reorder-animation.gif)
+
+## Usage
 
 ```jsx
 import { useReorder, Item } from 'use-reorder'
@@ -27,4 +30,25 @@ function MyComponent() {
     )
 }
 
+```
+
+## Hint animation
+
+Optionally, you can install [`framer-animations`](https://www.npmjs.com/package/framer-animations) to add this animation (useful as a simple guide for users)
+
+![User hint animation](media/reorder-animation.gif)
+
+```bash
+npm i framer-animations
+```
+
+```bash
+yarn add framer-animations
+```
+
+Exactly the same as `useReorder` but returns a `run` function to trigger the animation
+```jsx
+import { useAnimatedReorder } from 'use-reorder/dist/animated'
+
+const { reorderer, run } = useAnimatedReorder(items)
 ```
